4ede005334c1bcd225c9ce3c3462dad584bb3293
Jordan Luong <jordanw.luong@gmail.com>
2020-04-13 17:35:54 +0900

    [FEAT] complete play audio button and config

--- app/assets/javascripts/play_music.js
+++ app/assets/javascripts/play_music.js
@@ -1,14 +1,29 @@
-function playMusic() {
-  if (localStorage.getItem("music") === "false"){
-    var promise = document.getElementById('audio_landing').play();
-    if (promise !== undefined) {
-      promise.then(_ => {
-        // Autoplay started!
-        localStorage.setItem("music", true)
-      })
-    }
-  } else {
+const playMusic = () => {
+  if (localStorage.getItem("music") === null) {
     localStorage.setItem("music", false)
-    document.getElementById('audio_landing').pause()
+  }
+
+  const musicIcon = document.getElementById('musicIcon')
+  const musicStatus = localStorage.getItem("music") === "true" ? true : false;
+
+  if (musicStatus === true) {
+    document.getElementById('audio').play();
+
+    musicIcon.classList.remove("fa-volume-mute");
+    musicIcon.classList.add("fa-volume-up");
+  } else {
+    document.getElementById('audio').pause();
+
+    musicIcon.classList.remove("fa-volume-up");
+    musicIcon.classList.add("fa-volume-mute");
   }
 }
+
+const toggleMusic = () => {
+  let musicStatus = localStorage.getItem("music") === "true" ? true : false;
+
+  localStorage.setItem("music", !musicStatus);
+  playMusic();
+}
+
+window.onload = playMusic();
